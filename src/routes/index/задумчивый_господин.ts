0b6154f33a9395fe5b6e850d78da2b483b2977f4
EnergoStalin <ens.stalin@gmail.com>
2025-02-15 03:20:51 +0300

    refactor: moyai to mutation observer & handle window resizing properly

--- src/routes/index/задумчивый_господин.ts
+++ src/routes/index/задумчивый_господин.ts
@@ -1,7 +1,36 @@
-import { waitNotNull } from "@/utils/waitNotNull";
+const observer = new MutationObserver((records) => {
+	for (const r of records) {
+		const img = r.target as HTMLImageElement;
 
+		if (img.src.includes('/thinker-')) {
+			observer.disconnect()
+			img.src = "https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png"
+		}
+	}
+})
+
+function observe() {
+	observer.observe(document.body, {
+		subtree: true,
+		attributes: true,
+		attributeFilter: ["src"],
+	})
+}
+
+let oldwidth = window.visualViewport?.width
 export function задумчивый_господин() {
-	waitNotNull(() =>
-		document.querySelector("img[alt=\"decorative image\"]") as HTMLImageElement, 60000, 200
-	).then(e => e.src = "https://static.wikia.nocookie.net/surrealmemes/images/a/a0/Moyai.png")
+	observe()
+
+	// On width change reobserve
+	window.addEventListener("resize", () => {
+		const newwidth = window.visualViewport?.width
+		if(oldwidth !== newwidth) {
+			oldwidth = newwidth
+			observe()
+		}
+	})
+}
+
+export function перестать_думать() {
+	observer.disconnect()
 }
